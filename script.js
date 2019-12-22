efd567193d629b7773f96d26bc727fb2760017ce
tharllymartins <tharllymartins16@gmail.com>
2019-12-22 16:26:36 -0300

    new actions JS in introduction

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 let search = document.getElementById('search')
+let btnclik = document.getElementById('earthbtn')
+let img = document.getElementById('imgwelcome')
+let msg = document.getElementById('msg1')
+let msg2 = document.getElementById('msg2')
 
 function pesquisar(){
     alert('Doesn´t working yet, sorry!')
+}
+
+function btnclick(){
+    btnclik.innerHTML = "You're Late Human!!"
+    msg.innerHTML = "Happy? look what u did"
+    msg2.innerHTML = "It's already destroyed"
+    img.src = "images/earth.jpeg"
+    
 }
